44478928f6cd06716885c60897009bc090bca509
thenaz <ammarznazir@gmail.com>
2017-02-07 09:42:27 -0700

    more editing yaaassss

--- src/recursion/model/RecursionTool.java
+++ src/recursion/model/RecursionTool.java
@@ -1,5 +1,18 @@
 package recursion.model;
 
-public class RecursionTool {
+public class RecursionTool
+{
 
+	
+	public int calculateFactorial(int currentNumber)
+	{
+		if(currentNumber == 0 || currentNumber == 1)
+		{
+			return 1;
+		}
+		else
+		{
+			return calculateFactorial(currentNumber - 1) * currentNumber;
+		}
+	}
 }
